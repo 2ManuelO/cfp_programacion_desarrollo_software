33084682537a46852622e89f898ca5be22acc45b
Marlon Manuel González Osorio <manuelgonzalez@CFPBARTOLOMEAMBROSIO.COM>
2024-05-27 16:05:35 -0600

    Se agregaron ejercicio del 36 al 41

--- Modulo2/Javascript/Clase/Ejercicio36.js
+++ Modulo2/Javascript/Clase/Ejercicio36.js
@@ -1,11 +1,12 @@
 let num = 2;
-let resultado = 0;
+let resultado = 0; 
 
-for (i = 0; i > 10; i++) {
-    if (i = 0) {
+for (potencia = 0; potencia <= 10; potencia++) {
+    if (potencia == 0) {
         resultado = 1;
     } else {
-        resultado = resultado + num;
-        console.log(`${num} potencia ${i} = ${resultado}`);
+        resultado = resultado * num;
     }
 }
+
+console.log(resultado);
